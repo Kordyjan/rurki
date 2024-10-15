9e315637073e606b169a8286daf9cc522ad21091
Paweł Marks <paweljmarks@gmail.com>
2024-10-16 00:05:02 +0200

    Implement debug for commands

--- simple_engine/commands.rs
+++ simple_engine/commands.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Debug, Formatter};
+
 use engine_base::operators::{
     types::{Type, Wrapper},
     InputRef,
@@ -13,6 +15,23 @@ pub enum Command {
     Shutdown,
     Listen(Apt, Box<dyn Listener + Send>),
     Emit(InputRef, Type, Box<dyn Emitter + Send>),
+}
+
+
+impl Debug for Command {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Command::Start => write!(f, "Start")?,
+            Command::Shutdown => write!(f, "Shutdown")?,
+            Command::Listen(signal, listener) => {
+                write!(f, "Listen({:?}, {:p})", signal, listener)?
+            }
+            Command::Emit(input, ty, emitter) => {
+                write!(f, "Emit({:?}, {:?}, {:p})", input, ty, emitter)?
+            }
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug)]
